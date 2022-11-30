cf0f24fdbf93427fe39faf7cd08e8f2eb7d63324
Grant Kemm <grant.kemm0@gmail.com>
2022-11-30 13:16:00 +0200

    Revert "Implemented the Edit button"
    This reverts commit 2a263c10cfc6d19512c7b85b0551d750b17290a3.

--- gui.py
+++ gui.py
@@ -4,12 +4,9 @@ import PySimpleGUI as sg
 label = sg.Text("Type in a task")
 input_box = sg.InputText(tooltip="Enter a Task", key="task")
 add_button = sg.Button("Add")
-list_box = sg.Listbox(values=functions.get_tasks(), key="tasks",
-                      enable_events=True, size=[45, 10])
-edit_button = sg.Button("Edit")
 
 window = sg.Window("My Tasks App",
-                   layout=[[label], [input_box, add_button], [list_box, edit_button]],
+                   layout=[[label], [input_box, add_button]],
                    font=("Helvetica", 12))
 
 while True:
@@ -22,21 +19,6 @@ while True:
             new_task = values['task'] + "\n"
             tasks.append(new_task)
             functions.write_tasks(tasks)
-            window['tasks'].update(values=tasks)
-
-        case "Edit":
-            task_to_edit = values['tasks'][0]
-            new_task = values['task']
-
-            tasks = functions.get_tasks()
-            index = tasks.index(task_to_edit)
-            tasks[index] = new_task
-            functions.write_tasks(tasks)
-            window['tasks'].update(values=tasks)
-
-        case "tasks":
-            window['task'].update(value=values['tasks'][0])
-
         case sg.WINDOW_CLOSED:
             break
 
