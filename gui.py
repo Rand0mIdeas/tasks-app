b506dbdfb293a308cac959cfa0c7f849cdd0bda8
Grant Kemm <grant.kemm0@gmail.com>
2022-12-05 09:28:49 +0200

    Implemented the complete and exit buttons

--- gui.py
+++ gui.py
@@ -7,9 +7,14 @@ add_button = sg.Button("Add")
 list_box = sg.Listbox(values=functions.get_tasks(), key="tasks",
                       enable_events=True, size=[45, 10])
 edit_button = sg.Button("Edit")
+complete_button = sg.Button("Complete")
+exit_button = sg.Button("Exit")
 
 window = sg.Window("My Tasks App",
-                   layout=[[label], [input_box, add_button], [list_box, edit_button]],
+                   layout=[[label],
+                           [input_box, add_button],
+                           [list_box, edit_button, complete_button],
+                           [exit_button]],
                    font=("Helvetica", 12))
 
 while True:
@@ -33,6 +38,17 @@ while True:
             tasks[index] = new_task
             functions.write_tasks(tasks)
             window['tasks'].update(values=tasks)
+
+        case "Complete":
+            task_to_complete = values['tasks'][0]
+            tasks = functions.get_tasks()
+            tasks.remove(task_to_complete)
+            functions.write_tasks()
+            window['taksks'].update(values=tasks)
+            window[task].update(value='')
+
+        case "Exit":
+            break
 
         case "tasks":
             window['task'].update(value=values['tasks'][0])
